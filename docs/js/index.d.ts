c1b4730c9f09f5500bc5deb4e48325714aac9755
Joe Hildebrand <joe-github@cursive.net>
2024-03-24 09:45:27 -0600

    Add wasm dds

--- docs/js/index.d.ts
+++
@@ -1,7 +0,0 @@
-declare const work: Worker;
-declare const hv: HTMLIFrameElement;
-declare const btn: HTMLButtonElement;
-declare const prev: HTMLButtonElement;
-declare const tries: HTMLSpanElement;
-declare function nextDeal(num?: BigInt): void;
-declare const hash: string;
--- src/worker.ts
+++ src/worker.ts
@@ -1,12 +1,67 @@
-import {Deal, findDeal, prec2d} from './card.js';
+// This isn't actually ES6, because I can't recompile the WASM wrapper.
 
-addEventListener('message', (e): void => {
-  let d: Deal | undefined = undefined;
-  let tries = 0;
-  if (e.data) {
-    d = new Deal(e.data);
-  } else {
-    [d, tries] = findDeal(prec2d);
-  }
-  postMessage({lin: d.lin(), num: d.num, tries});
+// import {Deal, findDeal, prec2d} from './card.js';
+
+// Has to be var for stinky non-modularized emscripten output.
+// eslint-disable-next-line no-var
+var Module: any = null;
+const wasmReady = new Promise<void>((resolve, reject) => {
+  // eslint-disable-next-line no-var
+  Module = {
+    onAbort(...args: any): void {
+      // eslint-disable-next-line no-console
+      console.error('Abort', ...args);
+    },
+    onRuntimeInitialized(): void {
+      resolve();
+    },
+  };
+  importScripts('./dds.js'); // Magic happens to add to Module.
 });
+
+Promise.all([import('./card.js'), wasmReady]).then(([{
+  Deal,
+  findDeal,
+  prec2d,
+}]) => {
+  const handleDDSRequest = Module.cwrap(
+    'handleDDSRequest',
+    'string',
+    [
+      'string',
+      'string',
+      'string',
+      'string',
+      'string',
+      'string',
+      'string',
+      'string',
+      'string',
+      'string',
+    ]
+  );
+
+  addEventListener('message', (e): void => {
+    let d: any = undefined;
+    let tries = 0;
+    if (e.data) {
+      d = new Deal(e.data);
+    } else {
+      [d, tries] = findDeal(prec2d);
+    }
+    postMessage({
+      num: d.num,
+      tries,
+    });
+
+    const pbn = d.pbn();
+
+    const tricks = JSON.parse(handleDDSRequest(pbn, null, 'm', null, 'None', null, null, null, d.num.toString(16), null));
+
+    postMessage(tricks);
+  });
+
+  postMessage('ready');
+// eslint-disable-next-line no-console
+}, console.error);
+
