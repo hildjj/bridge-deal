cd751646bf1b025ca3c299561be9534620f60717
Joe Hildebrand <joe-github@cursive.net>
2024-04-16 21:53:03 -0600

    compressionstream is inconsisten

--- test/encode.test.js
+++ test/encode.test.js
@@ -6,17 +6,21 @@ import {
 import {equal} from 'node:assert';
 import {test} from 'node:test';
 
+const txt = 'north\n  13-15, balanced\n  bid 1N: 13-15';
+
 test('bytesToBase64url', () => {
   const u8 = new Uint8Array([0xfb, 0xf6]);
   equal(bytesToBase64url(u8.buffer), '-_Y');
 });
 
 test('compressString', async() => {
-  const c = await compressString('north\n  13-15, balanced\n  bid 1N: 13-15');
-  equal(c, 'eJzLyy8qyeBSUDA01jU01VFISsxJzEtOTQGKJGWmKBj6WUFkANtJCkw');
+  const c = await compressString(txt);
+  // Incosistent output.
+  // equal(c, 'eJzLyy8qyeBSUDA01jU01VFISsxJzEtOTQGKJGWmKBj6WUFkANtJCkw');
+  equal(await decompressString(c), txt);
 });
 
 test('decompressString', async() => {
   const c = await decompressString('eJzLyy8qyeBSUDA01jU01VFISsxJzEtOTQGKJGWmKBj6WUFkANtJCkw');
-  equal(c, 'north\n  13-15, balanced\n  bid 1N: 13-15');
+  equal(c, txt);
 });
